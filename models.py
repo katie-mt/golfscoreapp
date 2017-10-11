e0c0a86accf55a9966460bddd2f6860a2e36579a
Jonathan James <jjamesnwa1011@gmail.com>
2017-10-10 18:44:45 -0700

    Changed order of objects in models to make more sense in terms of data hierarchy

--- models.py
+++ models.py
@@ -3,6 +3,20 @@ from app import db
 '''In java, classes usually go in their own file but for some reason the norm in python is to throw them
 all into one file called models so here they are...'''
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+    tournaments = db.relationship('Tournament', backref='owner')
+
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r' % self.username
+
 class Tournament(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
@@ -15,51 +29,6 @@ class Tournament(db.Model):
 
     def __repr__(self):
         return '<Tournament %r' % self.name
-
-
-class Score(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    round_id = db.Column(db.Integer,db.ForeignKey('round.id'))
-    hole_id = db.Column(db.Integer, db.ForeignKey('hole.id'))
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-    score = db.Column(db.Integer)
-
-    def __init__(self, round_id, hole_id, player_id, score):
-        self.round_id = round_id
-        self.hole_id = hole_id
-        self.player_id = player_id
-        self.score = score
-
-    def __repr__(self):
-        return '<Score %d' % self.score
-
-
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
-    db.relationship('Hole', backref = 'owner')
-
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return '<Course %r' % self.name
-
-class Hole(db.Model):
-    id = db.Column(db.Integer, primary_key= True)
-    par = db.Column(db.Integer)
-    owner_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-    scores = db.relationship('Score', backref='hole')
-
-
-    def __init__(self, owner_id, par):
-        self.owner_id = owner_id
-        self.par = par
-
-    def __repr__(self):
-        return '<Hole %d>' % self.id
-
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -86,16 +55,47 @@ class Round(db.Model):
     def __repr__(self):
         return '<Round %d>' % self.roundNum
 
-class User(db.Model):
+
+class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80))
-    tournaments = db.relationship('Tournament', backref='owner')
+    name = db.Column(db.String(120), unique=True)
+    db.relationship('Hole', backref = 'owner')
 
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self, name):
+        self.name = name
 
     def __repr__(self):
-        return '<User %r' % self.username
+        return '<Course %r' % self.name
+
+
+class Hole(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    par = db.Column(db.Integer)
+    owner_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    scores = db.relationship('Score', backref='hole')
+
+
+    def __init__(self, owner_id, par):
+        self.owner_id = owner_id
+        self.par = par
+
+    def __repr__(self):
+        return '<Hole %d>' % self.id
+
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    round_id = db.Column(db.Integer,db.ForeignKey('round.id'))
+    hole_id = db.Column(db.Integer, db.ForeignKey('hole.id'))
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
+    score = db.Column(db.Integer)
+
+    def __init__(self, round_id, hole_id, player_id, score):
+        self.round_id = round_id
+        self.hole_id = hole_id
+        self.player_id = player_id
+        self.score = score
+
+    def __repr__(self):
+        return '<Score %d' % self.score
