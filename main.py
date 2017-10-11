2a8f531ac13c6ce8726b902477b4270fa0c41b8b
Dan Rodrigues <Danieljosephrodrigues@gmail.com>
2017-10-10 20:21:39 -0700

    created index controller

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from models import User, Tournament, Player, Round, Course, Hole, Score
 
 @app.route("/")
 def index():
-    return "Hello!"
+   encoded_error = request.args.get("error")
+   return render_template("signin.html", error=encoded_error and cgi.escape(encoded_error, quote=True))
+
 
 
 
